c9909ecf657f6b4afdf38a7e7e0908890b910032
Raphael Berger <raphaelmb@gmail.com>
2022-06-12 00:22:38 -0300

    Second commit

--- main.go
+++ main.go
@@ -5,13 +5,42 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", handlerFunc)
-	fmt.Println("Starting server on port 3000.")
-	http.ListenAndServe(":3000", nil)
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>This is the home page</h1>")
 }
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Hello</h1>")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>Email us at <a href=\"mailto:email@email.com\">email@email.com</a></p>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+	<ul>
+		<li>Question? Answer</li>
+	</ul>
+`)
+}
+
+type Router struct {}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		http.Error(w, "Page not found.", http.StatusNotFound)
+	}
+}
+
+func main() {
+	var router Router
+	fmt.Println("Starting server on port 3000.")
+	http.ListenAndServe(":3000", router)
 }
