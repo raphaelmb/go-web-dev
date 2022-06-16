fc922c764498f812a5c2a0e09341e71403b48536
Raphael Berger <raphaelmb@gmail.com>
2022-06-16 14:51:17 -0300

    Embedding template files + starting with tailwind

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/raphaelmb/go-web-dev/views"
@@ -9,5 +10,32 @@ import (
 func StaticHandlder(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ (tpl views.Template) http.HandlerFunc {
+	questions := []struct{
+		Question string
+		Answer template.HTML
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer: "Yes.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer: "24/7.",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer: `Via email - <a href="mailto:email@email.com">support@email.com</a>`,
+		},
+		{
+			Question: "Where is your office located?",
+			Answer: "Our entire team is remote.",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
--- main.go
+++ main.go
@@ -3,25 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/raphaelmb/go-web-dev/controllers"
+	"github.com/raphaelmb/go-web-dev/templates"
 	"github.com/raphaelmb/go-web-dev/views"
 )
 
 func main() {
 	r := chi.NewRouter()
-	
-	r.Get("/", controllers.StaticHandlder(views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))))
 
-	r.Get("/contact", controllers.StaticHandlder(views.Must(views.Parse(filepath.Join("templates", "contact.gohtml"))))) 
+	r.Get("/", controllers.StaticHandlder(views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
 
-	r.Get("/faq", controllers.StaticHandlder(views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))))
+	r.Get("/contact", controllers.StaticHandlder(views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
+
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "Page not found", http.StatusNotFound) 
+		http.Error(w, "Page not found", http.StatusNotFound)
 	})
+	
 	fmt.Println("Starting server on port 3000.")
 	http.ListenAndServe(":3000", r)
 }
